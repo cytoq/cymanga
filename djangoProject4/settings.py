4f0c3044ed226dca06600b537188410fb47aa2fe
cytoq <123martingospodinov@gmail.com>
2024-12-12 04:49:10 +0200

    Update settings.py

--- djangoProject4/settings.py
+++ djangoProject4/settings.py
@@ -8,8 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6=)ayi9z=ydsd5^t%-k^%m&s3h6aft^af-x+h%)89j6ui!izdi'
+#im not showing you my secret key (it's really secret)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -62,17 +61,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'djangoProject4.wsgi.application'
 
 
-DATABASES = {
-   "default": {
-       "ENGINE": "django.db.backends.postgresql",
-     "NAME": "cymanga",
-       "USER": "postgres",
-      "PASSWORD": "Eresunmono123",
-      "HOST": "127.0.0.1",
-      "PORT": "5432",
-     }
- }
-
+#not showing you my postgres password either, add your own :P
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
