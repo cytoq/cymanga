831944ecc750e283eed2daafc25510ff69454c6c
cytoq <123martingospodinov@gmail.com>
2024-12-20 03:09:53 +0200

    removed an unused import

--- manga/views.py
+++ manga/views.py
@@ -1,4 +1,3 @@
-from django.contrib.auth.mixins import UserPassesTestMixin
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import DeleteView, ListView, CreateView, UpdateView
 from django.urls import reverse_lazy
